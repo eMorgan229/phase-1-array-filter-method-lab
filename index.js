29d0b51b21fe462799fbcee96701d6289c9b644e
Em <ekramm@me.com>
2022-04-29 12:46:13 -0400

    commit tree 99526db7476a09402bf247e71bf943c6d58a3893

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+
+function findMatching(array, inputString) {
+    return array.filter(
+     (possibleMatchString => possibleMatchString.toLowerCase() === inputString.toLowerCase())    
+     );
+  }
+
+
+ function fuzzyMatch(array, inputString) {
+     return array.filter(
+        (possibleMatchingString => possibleMatchingString.startsWith(inputString))
+    );
+ }
+     
+
+function matchName(array, inputString) {
+
+    return array.filter(
+         (possibleMatchString => possibleMatchString.name === inputString)  
+    );
+ }
+
